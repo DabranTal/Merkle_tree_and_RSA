b9504c4d0e7b6d726cda49d9edb5cd0537449a57
Tal Dabran <dabrantal@gmail.com>
2022-06-16 23:31:36 +0300

    1 and 2 almodt done

--- merkle.py
+++ merkle.py
@@ -18,8 +18,15 @@ class Node:
 
     def update_node_val(self):
         if self.left and self.right:
-            con = self.left.val + self.left.val
-            self.val = hashlib.sha256(con.encode('utf-8'))
+            con = self.left.val + self.right.val
+            #self.val = hashlib.sha256(con.encode('utf-8'))
+            self.val = con
+
+
+    def upgrade_parents(self):
+        if self:
+            self.update_node_val()
+            self.parent.update_node_val()
 
     def is_leaf(self):
         if not self.left and not self.right:
@@ -37,7 +44,6 @@ class Node:
         if self.is_complete:
             right_node = Node(val, self.height)
             #new_parent = Node(self.val.hexdigest() + right_node.val.hexdigest(), self.height + 1)
-
             new_parent = Node(self.val + right_node.val, self.height + 1)
             new_parent.left = self
             new_parent.right = right_node
@@ -46,26 +52,35 @@ class Node:
             right_node.parent = new_parent
             if right_node.is_complete:
                 new_parent.is_complete = True
-            new_parent.down_grade_children()
+            else:
+                new_parent.is_complete = False
             # return the new root
             return new_parent
         # case leaf need to be added below
         else:
-            insert_here = self.find_where_insert
+            insert_here = find_where_insert(self)
             if not insert_here:
                 insert_here = self
             right_node = Node(val, insert_here.height - 1)
             #new_parent = Node(insert_here.val.hexdigest() + right_node.val.hexdigest(), insert_here.height)
-            new_parent = Node(insert_here.val+ right_node.val, insert_here.height)
+            new_parent = Node(insert_here.val + right_node.val, insert_here.height)
+            insert_here.height -= 1
+            # add children to new node
             new_parent.left = insert_here
             new_parent.right = right_node
-            # update parents
+            # update parent to point node's new parent
+            if insert_here.parent.left.val == insert_here.val:
+                insert_here.parent.left = new_parent
+            else:
+                insert_here.parent.right = new_parent
+            # update parents to new nodes
             new_parent.parent = insert_here.parent
             insert_here.parent = new_parent
             right_node.parent = new_parent
             # update children
             insert_here.down_grade_children()
             # return the root
+            new_parent.upgrade_parents()
             return self
 
     # down grade all the children below change is_complete if after downgrade sub-tree became complete
@@ -82,14 +97,45 @@ class Node:
         self.is_complete = le and ri
         return le and ri
 
-    # left order scanning to know where to insert the new node
-    def find_where_insert(self):
+    def update_is_complete(self):
         if self.is_complete:
+            return True
+        if self.height == 0:
+            self.is_complete = True
+            return True
+        le, ri = False, False
+        if self.left:
+            le = self.left.update_is_complete()
+        if self.right:
+            ri = self.right.update_is_complete()
+        if le and ri:
+            self.is_complete = True
+        return le and ri
+
+# left order scanning to know where to insert the new node
+def find_where_insert(node):
+    if node.is_leaf():
+        if not node.is_complete:
+            return node
+        else:
             return None
-        ret = self.left.find_where_insert()
-        if not ret:
-            ret = self.right.find_where_insert()
-        return ret
+    if node.is_complete:
+        return None
+    left_ret = find_where_insert(node.left)
+    if not left_ret:
+        right_ret = find_where_insert(node.right)
+        if not right_ret:
+            return node
+        else:
+            return right_ret
+    else:
+        return left_ret
+
+def just_test_print(node):
+    if node:
+        node.print_node()
+        just_test_print(node.left)
+        just_test_print(node.right)
 
 if __name__ == '__main__':
     isTreeInit = False
@@ -113,6 +159,9 @@ if __name__ == '__main__':
             # case the tree initialized
             elif extra is not None:
                 root = root.add_node(extra)
+            root.update_is_complete()
+            if root.is_complete:
+                a = 7
 
         if command == '2':
-            root.print_node()
+            just_test_print(root)
